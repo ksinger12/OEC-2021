7ee88ba754091bb85b4a75771606e8f106bbde88
Andrew Fryer <andrew.fryer@queensu.ca>
2021-01-23 14:09:21 -0500

    test probabilities

---
+++ testProbabilites.py
@@ -0,0 +1,11 @@
+from probabilities import *
+
+print(probability_of_infection_in_class(30))
+print(probabiity_of_infection_in_class_teacher_and_ta(30))
+print(probabiity_of_infection_in_class_ta_and_student(30))
+print(probabiity_of_infection_in_class_teacher_and_student(30))
+print(probability_of_infection_switching_classes(30, 30))
+print(probability_of_infection_during_lunch_staff(10))
+print(probability_of_infection_during_lunch_same_grade(100))
+print(probability_of_infection_during_lunch_different_grade(560))
+print(probability_of_infection_during_extra_curricular("Band", 10))

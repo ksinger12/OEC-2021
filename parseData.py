dddd9de75a8c43f9ab58d4f67a82e8d5b285cf9d
Andrew Fryer <andrew.fryer@queensu.ca>
2021-01-23 11:20:57 -0500

    read in all sheets

--- parseData.py
+++ parseData.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
-data = pd.read_excel("OEC2021 - School Record Book .xlsx")
+data = [
+    pd.read_excel("OEC2021 - School Record Book .xlsx", sheet_name=0),
+    pd.read_excel("OEC2021 - School Record Book .xlsx", sheet_name=1),
+    pd.read_excel("OEC2021 - School Record Book .xlsx", sheet_name=2),
+    pd.read_excel("OEC2021 - School Record Book .xlsx", sheet_name=3),
+    ]
+
 
 print(data)
